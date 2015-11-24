d3a93759ee0a03224914e20bb737ee17281d541f
nextacademy <nextacademy@email.com>
2015-11-24 09:18:09 +0800

    assess

---
+++ assessment_ar_todos-master/config/application.rb
@@ -0,0 +1,26 @@
+require 'pathname'
+require 'sqlite3'
+require 'active_record'
+require 'logger'
+
+# Declaring constants
+APP_ROOT = Pathname.new(File.expand_path(File.join(File.dirname(__FILE__), '..')))
+APP_NAME = APP_ROOT.basename.to_s
+DB_PATH  = APP_ROOT.join('db', APP_NAME + ".db").to_s
+ActiveRecord::Base.logger = Logger.new(STDOUT) if ENV['DEBUG']
+
+
+# Basic Setup
+
+# Auto-load all models
+Dir[APP_ROOT.join('app', 'models', '*.rb')].each { |file| require file }
+
+# Auto-load all views
+Dir[APP_ROOT.join('app', 'views', '*.rb')].each { |file| require file }
+
+# Auto-load all controllers
+Dir[APP_ROOT.join('app', 'controllers', '*.rb')].each { |file| require file }
+
+# Establish database connection
+ActiveRecord::Base.establish_connection :adapter  => 'sqlite3',
+                                        :database => DB_PATH
---
+++ assessment_ar_todos-master/todo.rb
@@ -0,0 +1,7 @@
+# Pre-configurations (Do not change)
+require_relative 'config/application'
+
+
+# Your Code begins from this line onwards #
+app = Controller.new
+app.control
